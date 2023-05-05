18478ec27193a6d8dfcab098483378de5f13134a
kenkwanatgai <kjjkjjzyayufqza@yahoo.com>
2023-05-05 22:46:24 +0800

    a

--- jest.config.js
+++
@@ -1,3 +0,0 @@
-transformIgnorePatterns: [
-  'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|rollbar-react-native|@fortawesome|@react-native|@react-navigation)',
-];
